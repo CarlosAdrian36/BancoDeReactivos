0192be85b318887f0daa8dedb87331ea48c955fa
Carlos.Arroyo <carlos23_adrian@hotmail.com>
2026-03-05 10:43:18 -0600

    se agregro el top menu

--- src/modules/login/views/restaurar.vue
+++ src/modules/login/views/restaurar.vue
@@ -2,6 +2,7 @@
   import { ref } from 'vue'
   import { useForm } from 'vee-validate'
   import { z } from 'zod'
+  import { toTypedSchema } from '@vee-validate/zod'
 
   const resetearSchema = z
     .object({
@@ -21,17 +22,27 @@
       message: 'Las contraseñas no coinciden'
     })
 
-  const onSubmit = () => {
-    console.log('Aqui va algo para enviar la solicitud de restauración de contraseña')
-  }
+  type ReseteoForm = z.infer<typeof resetearSchema>
 
-  const { handleSubmit, errors, meta, values, defineField } = useForm({})
+  const { handleSubmit, errors, meta, values, defineField } = useForm<ReseteoForm>({
+    validationSchema: toTypedSchema(resetearSchema),
+    initialValues: {
+      contrasena: '',
+      confirmacion: ''
+    }
+  })
+  const onSubmit = handleSubmit(values => {
+    console.log('Formulario válido:', values)
+  })
+
+  const [contrasena, contrasenaAttrs] = defineField('contrasena')
+  const [confirmacion, confirmacionAttrs] = defineField('confirmacion')
 </script>
 
 <template>
   <div class="w-full max-w-md sm:max-w-lg lg:max-w-xl">
     <div
-      class="glass-card bg-white/70 dark:bg-white/10 border border-white/30 dark:border-white/10 shadow-2xl rounded-3xl p-6 sm:p-8 lg:p-10 transition-all duration-300"
+      class="glass-card bg-white/30 dark:bg-white/10 border border-white/30 dark:border-white/10 shadow-2xl rounded-3xl p-6 sm:p-8 lg:p-10 transition-all duration-300"
     >
       <!-- HEADER -->
       <div class="flex flex-col items-center gap-6 mb-10">
@@ -58,7 +69,9 @@
       <form @submit.prevent="onSubmit" class="space-y-6">
         <!-- Usuario -->
         <div>
-          <label class="block text-sm font-medium text-base-content/30 mb-2"> Usuario </label>
+          <label class="block text-sm font-medium text-base-content/30 mb-2">
+            Nueva Contraseña
+          </label>
 
           <div class="relative group">
             <i
@@ -66,16 +79,66 @@
             ></i>
 
             <input
+              v-model="contrasena"
+              v-bind="contrasenaAttrs"
               type="text"
-              required
-              class="validator w-full pl-10 pr-4 py-3 bg-base-200/50 rounded-xl focus:ring-2 focus:ring-primary focus:border-primary text-base-content placeholder-gray-400 dark:placeholder-gray-500 transition-all outline-none"
+              :class="[
+                'w-full pl-10 pr-4 py-3 bg-base-200/50 rounded-xl focus:ring-2 focus:ring-primary focus:border-primary text-base-content placeholder-gray-400 dark:placeholder-gray-500 transition-all outline-none',
+                {
+                  'border border-red-500 focus:ring-red-500 focus:border-red-500': errors.contrasena
+                }
+              ]"
             />
+            <span class="text-red-500 text-sm absolute left-0 -bottom-5">
+              {{ errors.contrasena }}
+            </span>
+          </div>
+          <div class="mt-3 space-y-1 text-sm">
+            <div
+              class="flex items-center gap-2"
+              :class="values.contrasena?.length >= 8 ? 'text-green-500' : 'text-gray-400'"
+            >
+              <i class="fa-solid fa-check text-xs"></i>
+              <span>Mínimo 8 caracteres</span>
+            </div>
+
+            <div
+              class="flex items-center gap-2"
+              :class="/[A-Z]/.test(values.contrasena) ? 'text-green-500' : 'text-gray-400'"
+            >
+              <i class="fa-solid fa-check text-xs"></i>
+              <span>Al menos una letra mayúscula</span>
+            </div>
+
+            <div
+              class="flex items-center gap-2"
+              :class="/[a-z]/.test(values.contrasena) ? 'text-green-500' : 'text-gray-400'"
+            >
+              <i class="fa-solid fa-check text-xs"></i>
+              <span>Al menos una letra minúscula</span>
+            </div>
+
+            <div
+              class="flex items-center gap-2"
+              :class="/[0-9]/.test(values.contrasena) ? 'text-green-500' : 'text-gray-400'"
+            >
+              <i class="fa-solid fa-check text-xs"></i>
+              <span>Al menos un número</span>
+            </div>
+
+            <div
+              class="flex items-center gap-2"
+              :class="/[@$!%*?&]/.test(values.contrasena) ? 'text-green-500' : 'text-gray-400'"
+            >
+              <i class="fa-solid fa-check text-xs"></i>
+              <span>Un carácter especial (@, $, !, %, *, ?, &)</span>
+            </div>
           </div>
         </div>
         <!-- Correo -->
         <div>
           <label class="block text-sm font-medium text-base-content/30 mb-2">
-            Correo Electrónico
+            Confirmar Contraseña
           </label>
 
           <div class="relative group">
@@ -84,10 +147,20 @@
             ></i>
 
             <input
-              type="email"
-              required
-              class="validator w-full pl-10 pr-4 py-3 bg-base-200/50 rounded-xl focus:ring-2 focus:ring-primary focus:border-primary text-base-content placeholder-gray-400 dark:placeholder-gray-500 transition-all outline-none"
+              v-model="confirmacion"
+              v-bind="confirmacionAttrs"
+              type="password"
+              :class="[
+                'w-full pl-10 pr-4 py-3 bg-base-200/50 rounded-xl focus:ring-2 focus:ring-primary focus:border-primary text-base-content placeholder-gray-400 dark:placeholder-gray-500 transition-all outline-none',
+                {
+                  'border border-red-500 focus:ring-red-500 focus:border-red-500':
+                    errors.confirmacion
+                }
+              ]"
             />
+            <span class="text-red-500 text-sm absolute left-0 -bottom-5">
+              {{ errors.confirmacion }}
+            </span>
           </div>
         </div>
 
@@ -97,7 +170,7 @@
           type="submit"
           class="w-full py-4 bg-primary hover:bg-primary/90 text-white font-bold rounded-xl shadow-lg shadow-primary/30 transition-all active:scale-[0.98]"
         >
-          Enviar Solicitud
+          Reestablecer Contraseña
         </button>
       </form>
 
@@ -125,3 +198,9 @@
     </div>
   </div>
 </template>
+<style scoped>
+  .glass-card {
+    backdrop-filter: blur(16px) saturate(180%);
+    -webkit-backdrop-filter: blur(16px) saturate(180%);
+  }
+</style>
