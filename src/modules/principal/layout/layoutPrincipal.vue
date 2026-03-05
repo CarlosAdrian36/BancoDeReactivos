32299bc8ed9e03ad36936b165b1a6376045de408
Carlos.Arroyo <carlos23_adrian@hotmail.com>
2026-03-05 17:00:22 -0600

    layout de la pagina principal ademas con los routerviews funcionando

--- src/modules/principal/layout/layoutPrincipal.vue
+++ src/modules/principal/layout/layoutPrincipal.vue
@@ -12,22 +12,17 @@
 <template>
   <div class="flex flex-col h-screen overflow-hidden">
     <topMenu @toggle="toggleDrawer" />
-
-    <div class="drawer lg:drawer-open flex-1">
+    <div class="drawer lg:drawer-open">
       <input type="checkbox" class="drawer-toggle" v-model="isDrawerOpen" />
-
       <div class="drawer-content">
-        <div class="p-4"><RouterView /></div>
+        <div class="p-4"><router-view></router-view></div>
       </div>
 
       <div class="drawer-side">
-        <label class="drawer-overlay" aria-level="close sidebar"></label>
-
         <aside
           class="flex h-full flex-col bg-base-100 border-r border-primary/10 transition-all duration-300 shrink-0 overflow-hidden"
-          :class="isDrawerOpen ? 'w-80' : 'w-0'"
+          :class="isDrawerOpen ? 'w-80 opacity-100' : 'w-0 opacity-0 '"
         >
-          <!-- 🔹 Sección superior -->
           <div class="flex flex-col gap-1 p-6">
             <p
               class="px-3 text-[11px] font-bold uppercase tracking-widest text-primary/40 mb-3"
@@ -35,34 +30,27 @@
             >
               Navegacion
             </p>
-
-            <!-- Item -->
-
-            <button
+            <router-link
+              :to="{ name: 'Todo' }"
               class="flex items-center gap-3 rounded-xl px-3 py-2.5 text-sm font-medium transition-colors hover:bg-base-200 cursor-pointer"
             >
-              <!-- Espacio para icono -->
-              <div class="w-5 h-5">
-                <i class="fa-solid fa-grid-2"></i>
-              </div>
-              <span v-if="isDrawerOpen">Todo</span>
-            </button>
-
-            <button
+              <div class="w-5 h-5"><i class="fa-solid fa-grid-2"></i></div>
+              <span>Todo</span>
+            </router-link>
+            <router-link
+              :to="{ name: 'Carpetas' }"
               class="flex items-center gap-3 rounded-xl px-3 py-2.5 text-sm font-medium transition-colors hover:bg-base-200 cursor-pointer"
             >
               <div class="w-5 h-5"><i class="fa-sharp-duotone fa-solid fa-folder-open"></i></div>
-              <span v-if="isDrawerOpen">Carpetas</span>
-            </button>
-
-            <button
+              <span>Carpetas</span>
+            </router-link>
+            <router-link
+              :to="{ name: 'Gestion de Miembros' }"
               class="flex items-center gap-3 rounded-xl px-3 py-2.5 text-sm font-medium transition-colors hover:bg-base-200 cursor-pointer"
             >
-              <div class="w-5 h-5">
-                <i class="fa-solid fa-users"></i>
-              </div>
-              <span v-if="isDrawerOpen">Gestión de miembros del CAD</span>
-            </button>
+              <div class="w-5 h-5"><i class="fa-solid fa-users"></i></div>
+              <span>Gestion de Miembros</span>
+            </router-link>
           </div>
         </aside>
       </div>
